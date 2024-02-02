3cc97d85496ad118cbd95ba34e68956f43b6ff93
jordanfurr <ejfurr@umich.edu>
2024-02-01 20:37:18 -0500

    fixed?

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,9 +1,9 @@
 <template>
     <same-header/>
     <div class="flex flex-row w-100">
-    <information/>
-    <div class ="plr-15 flex flex-col flex-start w-60">
-      <p>*work*</p>
-    </div>
+        <information/>
+        <div class ="plr-15 flex flex-col flex-start w-60">
+            <p>*work*</p>
+        </div>
     </div>
 </template>
